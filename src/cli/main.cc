9a909f5e41254be6132573f5e3b43cbcc8f3e994
Farid Zakaria <fmzakari@google.com>
2023-05-18 20:19:16 +0000

    feat: add support for glog in codebase

--- src/cli/main.cc
+++ src/cli/main.cc
@@ -1,4 +1,5 @@
 #include "gflags/gflags.h"
+#include "glog/logging.h"
 
 DEFINE_bool(verbose, false, "Display program name before message");
 
@@ -6,6 +7,16 @@ int main(int argc, char** argv) {
   gflags::SetUsageMessage("Convert ELF to SQLite databases");
   gflags::SetVersionString("1.0.0");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  // to simplify our application lets log to STDERR rather than log files
+  // as this is a CLI application
+  FLAGS_logtostderr = true;
+
+  // Initialize Google’s logging library.
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  if (argc < 1) {}
+
   gflags::ShutDownCommandLineFlags();
   return 0;
 }
