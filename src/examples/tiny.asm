6ba1948589510929b12c4e6a95b2811426b48da3
Farid Zakaria <farid.m.zakaria@gmail.com>
2023-06-20 09:34:53 -0700

    fix: change int assembly to syscall in the examples (#3)
    * build: ignore log files in gitignore
    
    * fix: change int assembly to syscall in the examples

--- src/examples/tiny.asm
+++ src/examples/tiny.asm
@@ -1,8 +1,12 @@
 ; tiny.asm
-BITS 32
+BITS 64
 GLOBAL _start
 SECTION .text
 _start:
-              mov     eax, 1
-              mov     ebx, 42
-              int     0x80
+              ; Arch/ABI      arg1  arg2  arg3  arg4  arg5  arg6  arg7  Notes
+              ;  x86-64        rdi   rsi   rdx   r10   r8    r9    -
+              ; https://github.com/torvalds/linux/blob/v4.17/arch/x86/entry/syscalls/syscall_64.tbl#L71
+              mov     rax, 60
+              ; rdi is the value to put in the return value
+              mov     rdi, 42
+              syscall
